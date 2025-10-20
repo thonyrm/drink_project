94e4e52c3b5502489b8c6411f85ec6a9b613ba3f
Thony Dev <ramosm0106@gmail.com>
2025-10-20 06:39:10 -0300

    favorite slace created

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,59 +1,94 @@
-import { Dialog, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild, Description } from '@headlessui/react';
+import { Fragment, type JSX } from 'react';
 import { useAppStore } from '../store/useAppStore';
+import type { Recipe } from '../types';
 
 export default function Modal() {
     const modal = useAppStore( (state) => state.modal)
     const closeModal = useAppStore( (state) => state.closeModal)
     const selectedRecipe = useAppStore( (state) => state.selectedRecipe)
+
+    const renderIngridients = () => {
+        const ingredients: JSX.Element[] = []
+        for (let i =1 ; i <= 6 ; i++){
+            const ingredient = selectedRecipe[`strIngredient${i}` as keyof Recipe]
+            const measure = selectedRecipe[`strMeasure${i}` as keyof Recipe]
+            if (ingredient && measure){
+                ingredients.push(
+                    <li key={i} className='text-lg font-normal'>
+                        {ingredient} - {measure}
+                    </li>
+                )
+            }
+        }
+        return ingredients
+        
+    }
     return (
         <>
         <Transition appear show={modal} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={closeModal}>
-            <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-            >
-                <div className="fixed inset-0 bg-black/70" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 overflow-y-auto">
-                <div className="flex min-h-full items-center justify-center p-4 text-center">
-                <Transition.Child
+                <TransitionChild 
                     as={Fragment}
                     enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
                     leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
                 >
-                    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
-                    <Dialog.Title as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
-                        {selectedRecipe.strDrink}
-                    </Dialog.Title>
-                        <img 
-                            src={selectedRecipe.strDrinkThumb} 
-                            alt={`Imagen de ${selectedRecipe.strDrink}` }
-                            className='mx-auto w-96'
-                        />
-                    <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                        Ingredientes y Cantidades
-                    </Dialog.Title>
-                    <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                        {
-                            selectedRecipe.strInstructions
-                        }
-                    </Dialog.Title>
-                    </Dialog.Panel>
-                </Transition.Child>
+                    <div className="fixed inset-0 bg-black/70" />
+                </TransitionChild >
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <TransitionChild 
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
+                                <DialogTitle as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
+                                    {selectedRecipe.strDrink}
+                                </DialogTitle>
+                                    <img 
+                                        src={selectedRecipe.strDrinkThumb} 
+                                        alt={`Imagen de ${selectedRecipe.strDrink}` }
+                                        className='mx-auto w-96'
+                                    />
+                                <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
+                                    Ingredientes y Cantidades
+                                </DialogTitle>
+                                {renderIngridients()}
+                                <Description as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
+                                    Instrucciones
+                                </Description>
+                                <p>{ selectedRecipe.strInstructions }</p>
+                                <div className='mt-5 flex justify-between gap-4'>
+                                    <button
+                                        type='button'
+                                        className='w-full rounded bg-gray-600 p-3 font-bold uppercase text-white shadow hover:bg-gray-500 cursor-pointer'
+                                        onClick={closeModal}
+                                    >
+                                        Cerrar
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500 cursor-pointer'
+                                        
+                                    >
+
+                                    </button>
+                                </div>
+                                
+                            </DialogPanel>
+                        </TransitionChild >
+                    </div>
                 </div>
-            </div>
             </Dialog>
         </Transition>
         </>
--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,8 +1,9 @@
 import {create} from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, type RecipesSliceType } from './recipeSlice'
+import {type FavoritesSliceType, createFavoritesSlice} from './favoritesSlice'
 
-
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipeSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
 })))
