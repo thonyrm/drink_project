41487e88a24892d78ffbabf337333d1eb22b7155
Thony Dev <ramosm0106@gmail.com>
2025-10-22 06:11:29 -0300

    Show Favorites

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -2,8 +2,10 @@ import {create} from 'zustand'
 import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, type RecipesSliceType } from './recipeSlice'
 import {type FavoritesSliceType, createFavoritesSlice} from './favoritesSlice'
+import { createNotificationSlice, type NotificationSliceType } from './notificationSlice'
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a)
 })))
