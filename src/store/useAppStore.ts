a90fb4e48854a12957b91cfa7654edf7d75ff670
Thony Dev <ramosm0106@gmail.com>
2025-10-23 07:49:49 -0300

    Setting for IA components

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -3,9 +3,11 @@ import { devtools } from 'zustand/middleware'
 import { createRecipeSlice, type RecipesSliceType } from './recipeSlice'
 import {type FavoritesSliceType, createFavoritesSlice} from './favoritesSlice'
 import { createNotificationSlice, type NotificationSliceType } from './notificationSlice'
+import { createAISlice, type AISlice } from './aiSlice'
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
-    ...createNotificationSlice(...a)
+    ...createNotificationSlice(...a),
+    ...createAISlice(...a),
 })))
--- src/views/GenerateAI.tsx
+++ src/views/GenerateAI.tsx
@@ -1,27 +1,49 @@
+import type { FormEvent } from "react"
+import { useAppStore } from "../store/useAppStore"
 
 export default function GenerateAI() {
-  
-  
-  return (
+    
+    const showNotification = useAppStore((state)=> state.showNotification)
+    const generateRecipe = useAppStore((state)=> state.generateRecipe)
+    const recipe = useAppStore((state)=> state.recipe)
+    const isGenerating = useAppStore((state)=> state.isGenerating)
+
+    const handleSubmit = async( e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const prompt = form.get('prompt')?.toString()!
+        if(prompt.trim() === ''){
+            showNotification({
+                text: 'La búsqueda no puede ir vacía',
+                error: true
+            })
+            return
+        }
+        await generateRecipe(prompt)
+        console.log("Desde handlesubmit")
+    }
+    return (
         <>
         <h1 className="text-6xl font-extrabold">Generar Receta con IA</h1>
 
         <div className="max-w-4xl mx-auto">
             <form  
-            onSubmit={() => {}}
-            className='flex flex-col space-y-3 py-10'
+                onSubmit={handleSubmit}
+                className='flex flex-col space-y-3 py-10'
             >
             <div className="relative">
                 <input 
-                name="prompt" 
-                id="prompt" 
-                className="border bg-white p-4 rounded-lg w-full border-slate-800" 
-                placeholder="Genera una receta con ingredientes. Ej. Bebida con Tequila y Fresa"
+                    name="prompt" 
+                    id="prompt" 
+                    className="border bg-white p-4 rounded-lg w-full border-slate-800" 
+                    placeholder="Genera una receta con ingredientes. Ej. Bebida con Tequila y Fresa"
                 />
                 <button 
-                type="submit" 
-                aria-label="Enviar"
-                className={`cursor-pointer absolute top-1/2 right-5 transform -translate-x-1/2 -translate-y-1/2`}
+                    type="submit" 
+                    aria-label="Enviar"
+                    className={`cursor-pointer absolute top-1/2 right-5 transform -translate-x-1/2 -translate-y-1/2
+                        ${isGenerating ? "cursor-not-allowed opacity-50 " : ""}`}
+                    disabled= {isGenerating}
                 >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                     stroke="currentColor" className="w-10 h-10">
@@ -31,8 +53,9 @@ export default function GenerateAI() {
                 </button>
             </div>
             </form>
-
+            {isGenerating && <p className="text-center animate-pulse">Generando...</p> }
             <div className="py-10 whitespace-pre-wrap">
+                {recipe}
 
             </div>
         </div>
