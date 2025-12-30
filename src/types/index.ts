c6fa7ca54d650549c3d5713f594c7ae1645289b8
Anthony Ramos Morales <100253179+thonyrm@users.noreply.github.com>
2025-12-30 06:23:31 -0300

    Remove unnecessary blank line in index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,6 @@
 import {z} from 'zod'
 import type { CategoriesAPIResonseSchema, DrinkAPIResponse, DrinksAPIResponse, RecipeAPIResponseSchema, SearchFilterSchema } from '../utils/recipes-schema'
 
-
 export type Categories = z.infer<typeof CategoriesAPIResonseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
